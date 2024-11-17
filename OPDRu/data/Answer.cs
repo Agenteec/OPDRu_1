da1cbf14bcb4dee01c5a7ef1d20d0fa46153724a
Max <Max@DESKTOP-6MESG1N>
2024-11-17 21:46:58 +0300

    Были добавлены модели базы данных, сервис базы данных и тестов, были загружены библиотеки (Json, SQLite3)

---
+++ OPDRu/data/Answer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPDRu.data
+{
+    public class Answer
+    {
+        public string Text {  get; set; }
+        public bool Validation {  get; set; }
+    }
+}
